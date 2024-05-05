0b06ca85f872a5ffa13bdd28dba3f9978a6418f3
venkata pradeep <pvenkatapradeep4@gmail.com>
2024-05-05 17:47:14 +0530

    laptops added

--- index.js
+++
@@ -1,7 +0,0 @@
- function showPrice(productName) {
-  alert(`Price of ${productName}: $XX.XX`); // Replace $XX.XX with actual price
-}
-
-function addToCart(productName) {
-  alert(`${productName} added to cart!`);
-}
