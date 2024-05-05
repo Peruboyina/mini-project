e20d697e2805286464fb2b47a9f469b40b658fbb
venkata pradeep <pvenkatapradeep4@gmail.com>
2024-05-05 13:02:27 +0530

    small

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
- 
+ function showPrice(productName) {
+  alert(`Price of ${productName}: $XX.XX`); // Replace $XX.XX with actual price
+}
+
+function addToCart(productName) {
+  alert(`${productName} added to cart!`);
+}
